f9bbbcd45ec7f3a9e8af68c7f600dc85cd4504cf
Rach <54503978+justrach@users.noreply.github.com>
2025-09-07 14:02:59 +0800

    working code

--- apps/gemini-hackathon/next.config.ts
+++ apps/gemini-hackathon/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  transpilePackages: ['corlena', '@corlena/wasm']
 };
 
 export default nextConfig;
